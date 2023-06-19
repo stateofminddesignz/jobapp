2366028f6e7676e13e45b70bf583eac3097e8963
Larry McCullough <larry.mccullough88@gmail.com>
2023-06-19 03:19:50 -0400

    first commit to job app

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { View, Text,TextInput,TouchableOpacity,Image,FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import {icons,SIZES} from '../../../constants'
+
+
+const Welcome = () => {
+  const router = useRouter();
+  return (
+    <View>
+     <View style={styles.container} >
+       <Text style={styles.userName} >Hello Larry</Text>
+       <Text style={styles.welcomeMessage} >Find Your Perfect Job</Text>
+     </View>
+
+     <View styles={styles.searchContainer} >
+      <View style={styles.searchWrapper} >
+        
+        <TextInput
+           style={styles.searchInput}  
+           value=""
+           onChange={()=> {}}
+           placeholder='What are you looking for ?'
+           />
+        </View>
+       
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+           resizeMode='contain'
+            style={styles.searchBtnImage}
+              />
+            
+        </TouchableOpacity>
+      
+       
+
+     
+     </View>
+     
+
+    </View>
+  )
+}
+
+export default Welcome
